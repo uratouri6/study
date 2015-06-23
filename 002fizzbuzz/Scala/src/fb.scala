a225fd5327fcabbf358f8417da3f368ca41d5494
unknown <uro6uro@gmail.com>
2015-06-23 19:33:54 +0900

    20150623-2

---
+++ 002fizzbuzz/Scala/src/fb.scala
@@ -0,0 +1,22 @@
+object FizzBuzz extends App {
+
+  fizzbuzz(100)
+
+  def fizzbuzz(n:Int) {
+    for (i: Int <- 1 to n) {
+      if (i % 3 == 0 && i % 5 == 0) {
+        println("FizzBuzz")
+      }
+      else if (i % 3 == 0) {
+        println("Fizz")
+      }
+      else if (i % 5 == 0) {
+        println("Buzz")
+      }
+      else {
+        println(i)
+      }
+    }
+  }
+
+}
